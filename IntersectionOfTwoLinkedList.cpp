1cc60c7619226a9b4ebb85ad14f02c1fbb849d03
AryanSharma47 <161364791+AryanSharma47@users.noreply.github.com>
2024-02-29 23:01:55 +0530

    IntersectionOfTwoLinkedList.cpp

--- IntersectionOfTwoLinkedList.cpp
+++ IntersectionOfTwoLinkedList.cpp
@@ -19,3 +19,23 @@ public:
         return temp1;
     }
 };
+
+//Second Approach || Time Complexity : O(m*n) || Space Complexity : O(1)
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+
+        while(headA!=NULL)
+        {
+            ListNode *temp=headB;
+            while(temp!=NULL)
+            {
+                if(temp==headA)
+                return temp;
+                temp=temp->next;
+            }
+            headA=headA->next;
+        }
+        return NULL;
+    }
+};
