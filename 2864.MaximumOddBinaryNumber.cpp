a8a36746ff936381f75f5a4cf0c3f30d23a15c72
AryanSharma47 <161364791+AryanSharma47@users.noreply.github.com>
2024-03-01 11:03:48 +0530

    2864.MaximumOddBinaryNumber.cpp

--- 2864.MaximumOddBinaryNumber.cpp
+++ 2864.MaximumOddBinaryNumber.cpp
@@ -9,3 +9,16 @@ public:
         return s;
     }
 }; 
+
+//Second Approach
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        sort(s.begin(),s.end(),greater<int>()); // Sort in descending order
+        for(int i=s.size()-1;i>=0;i--)
+            if(s[i]=='1')
+            swap(s[i],s[s.size()-1]);
+        
+        return s;
+    }
+};
