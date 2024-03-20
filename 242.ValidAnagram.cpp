e2ad95fa89a68f93bdc4dc13e41712dd25c1b77e
AryanSharma47 <161364791+AryanSharma47@users.noreply.github.com>
2024-03-20 15:30:27 +0530

    242.ValidAnagram.cpp

--- 242.ValidAnagram.cpp
+++ 242.ValidAnagram.cpp
@@ -24,3 +24,21 @@ public:
         return true;
     }
 };
+
+//Second Approach using Sort function
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        int n=max(s.size(),t.size());
+
+        for(int i=0;i<n;i++)
+        {
+           if(s[i]!=t[i])
+            return false;
+        }
+        return true;
+    }
+};
