b42fef21277776aa500c578def0c42db977bcd47
AryanSharma47 <161364791+AryanSharma47@users.noreply.github.com>
2024-03-25 12:13:58 +0530

    442.FindDuplicatesInAnArray.cpp

--- 442.FindDuplicatesInAnArray.cpp
+++ 442.FindDuplicatesInAnArray.cpp
@@ -16,3 +16,21 @@ public:
         return v;
     }
 };
+
+//Second Approach Using integers Range
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> v;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            int ind=abs(nums[i])-1;
+            if(nums[ind] < 0)
+            v.push_back(ind+1);
+            else
+            nums[ind]*=-1;
+        }
+        return v;
+    }
+};
